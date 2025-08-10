53eb38f507ef8f79e2f35a1876fc4615bc0a211b
agus-darmawan <wayanagus.dr@gmail.com>
2025-08-11 05:51:46 +0700

    feat: add court api

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,18 @@ import {
   uploadHeadersMiddleware,
   uploadStatic,
 } from '@/middleware/upload.middleware.js';
+import v1Routes from '@/routes/index.js';
 
 const createApp = (): express.Application => {
   const app = express();
 
   setupMiddleware(app);
 
+  app.use('/api', v1Routes);
+
   app.get('/', (_req: Request, res: Response) => {
     res.status(200).json({
-      message: 'Welcome to the Patrol BE API',
+      message: 'Welcome to the Padel BE API',
       timestamp: new Date().toISOString(),
       author: 'I Wayan Agus Darmawan',
       version: '1.0.0',
